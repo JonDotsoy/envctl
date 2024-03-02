28d4d9dcd9c007d7fe9bd382a9119c060b224760
Jonathan Delgado <hi@jon.soy>
2024-03-02 19:23:07 -0300

    feat: Add Envctl class for enhanced .env file handling
    This commit introduces the Envctl class, a comprehensive tool designed to improve the management and switching of environment (.env) files within project workspaces. Key functionalities include the ability to find and differentiate between environment files and templates, a mechanism to switch between different environment configurations by merging template files with selected .env files, and utilities for identifying environment files based on naming conventions.
    
    Significant features include:
    - Dynamic discovery of .env and template files using glob patterns, supporting both root and nested locations (.envs directory).
    - An innovative switchEnv method that intelligently merges the chosen environment file with a template, facilitating flexible and dynamic configuration changes.
    - Support for multiple naming conventions for environment files, allowing for versatile project structures.
    
    This enhancement is poised to benefit developers by offering a more structured and efficient way to manage environment configurations, especially in complex projects with multiple deployment environments or configurations.

---
+++ src/envctl.ts
@@ -0,0 +1,112 @@
+import fs from "fs/promises";
+import dotenv from "dotenv";
+import { Glob, type GlobScanOptions } from "bun";
+
+export class Envctl {
+  workspaceLocation: URL;
+  templateLocation: URL;
+  envDestination: URL;
+
+  constructor(options: {
+    workspace: URL;
+    template?: URL;
+    envDestination?: URL;
+  }) {
+    this.workspaceLocation = options.workspace;
+    this.envDestination =
+      options.envDestination ?? new URL(".env", this.workspaceLocation);
+    this.templateLocation = options.template ?? this.envDestination;
+  }
+
+  private async *findEnvsStr() {
+    const scanOptions: GlobScanOptions = {
+      cwd: this.workspaceLocation.pathname,
+      dot: true,
+      onlyFiles: true,
+      absolute: true,
+    };
+
+    yield* new Glob("{.*.env,.env.*}").scan(scanOptions);
+    yield* new Glob(".envs/*").scan(scanOptions);
+  }
+
+  private async *findTemplatesStr() {
+    const scanOptions: GlobScanOptions = {
+      cwd: this.workspaceLocation.pathname,
+      dot: true,
+      onlyFiles: true,
+      absolute: true,
+    };
+
+    yield* new Glob(".env.template").scan(scanOptions);
+    yield* new Glob(".envs/template").scan(scanOptions);
+  }
+
+  async *findEnvs() {
+    const templatePaths = await Array.fromAsync(this.findTemplatesStr());
+
+    for await (const envStr of this.findEnvsStr()) {
+      if (templatePaths.includes(envStr)) continue;
+      yield new URL(envStr, this.workspaceLocation);
+    }
+  }
+
+  async switchEnv(envLocation: URL) {
+    const envOriginSource = await fs.readFile(envLocation);
+    const templateSource = await fs.readFile(this.templateLocation);
+    const envOriginParsed = dotenv.parse(envOriginSource);
+
+    const nextEnvPayload = new TextEncoder().encode(
+      new TextDecoder()
+        .decode(templateSource)
+        .replace(/^((?:\w|_)*)=(.*)/gm, (_: string, varName: string) => {
+          return `${varName}=${envOriginParsed[varName] ?? ""}`;
+        }),
+    );
+
+    await fs.writeFile(this.envDestination, nextEnvPayload);
+  }
+
+  static *toAlternativesNames(base: URL, name: string) {
+    yield new URL(`${name}`, base);
+    yield new URL(`.env.${name}`, base);
+    yield new URL(`.${name}.env`, base);
+    yield new URL(`.envs/${name}`, base);
+    yield new URL(`.envs/.${name}`, base);
+    yield new URL(`.envs/${name}.env`, base);
+    yield new URL(`.envs/.${name}.env`, base);
+  }
+
+  static getNameEnvFromLocation(location: URL) {
+    const rules = [
+      /\.env\.(?<name>\w+)$/,
+      /\.(?<name>\w+)\.env$/,
+      /(?<name>\w+)\.env$/,
+      /\.envs\/(?<name>\w+)\.env$/,
+      /\.envs\/.(?<name>\w+)\.env$/,
+      /\.envs\/.(?<name>\w+)$/,
+      /\.envs\/(?<name>\w+)$/,
+    ];
+
+    for (const rule of rules) {
+      if (rule.test(location.pathname)) {
+        return rule.exec(location.pathname)!.groups!.name;
+      }
+    }
+
+    return null;
+  }
+
+  static async findEnvLocation(base: URL, name: string) {
+    for (const alternativeName of this.toAlternativesNames(base, name)) {
+      if (
+        (await fs.exists(alternativeName)) &&
+        (await fs.stat(alternativeName)).isFile()
+      ) {
+        return alternativeName;
+      }
+    }
+
+    return null;
+  }
+}
